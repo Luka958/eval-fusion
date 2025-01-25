2ee908988291a4bceac9ea3fe3016e88aa163792
Luka958 <lp53523@fer.hr>
2025-01-25 12:51:47 +0100

    implemented TruLensEvaluator

--- libs/community/trulens/eval_fusion_trulens/evaluator.py
+++ libs/community/trulens/eval_fusion_trulens/evaluator.py
@@ -7,8 +7,9 @@ from eval_fusion_core.models import (
     EvaluationOutput,
     EvaluationOutputEntry,
 )
-from trulens.apps.virtual import TruVirtual, VirtualApp
-from trulens.core import Feedback
+from trulens.apps.virtual import TruVirtual, VirtualApp, VirtualRecord
+from trulens.core import Feedback, FeedbackMode, Select, TruSession
+from trulens.core.schema.feedback import FeedbackResult, FeedbackResultStatus
 
 from .llm import TruLensProxyLLM
 
@@ -21,31 +22,78 @@ class TruLensEvaluator(EvalFusionBaseEvaluator):
         self, inputs: list[EvaluationInput], metrics: list
     ) -> list[EvaluationOutput]:
         # TODO organize metrics
+        retriever = Select.RecordCalls.retriever
+        synthesizer = Select.RecordCalls.synthesizer
 
-        virtual_app = VirtualApp()
-        context = VirtualApp.select_context()
+        virtual_records = [
+            VirtualRecord(
+                main_input=x.input,
+                main_output=x.output,
+                calls={
+                    retriever.get_context: dict(args=[x.input], rets=x.relevant_chunks),
+                    synthesizer.generate: dict(args=[x.input], rets=[x.output]),
+                },
+            )
+            for x in inputs
+        ]
 
-        f_context_relevance = (
+        context_selector = retriever.get_context.rets[:]
+        output_selector = synthesizer.generate.rets[:]
+
+        context_relevance_feedback = (
             Feedback(
-                self.llm.context_relevance_with_cot_reasons, name='Context Relevance'
+                self.llm.context_relevance_with_cot_reasons, name='context_relevance'
             )
-            .on_input()
-            .on(context)
+            .on(Select.RecordInput)
+            .on(context_selector)
         )
-        f_groundedness = (
+        groundedness_feedback = (
             Feedback(
-                self.llm.groundedness_measure_with_cot_reasons, name='Groundedness'
+                self.llm.groundedness_measure_with_cot_reasons, name='groundedness'
             )
-            .on(context.collect())
-            .on_output()
+            .on(context_selector.collect())
+            .on(output_selector)
         )
-        f_qa_relevance = Feedback(
-            self.llm.relevance_with_cot_reasons, name='Answer Relevance'
+        answer_relevance_feedback = Feedback(
+            self.llm.relevance_with_cot_reasons, name='answer_relevance'
         ).on_input_output()
 
-        virtual_recorder = TruVirtual(
-            app_name='RAG',
-            app_version='simple',
-            app=virtual_app,
-            feedbacks=[f_context_relevance, f_groundedness, f_qa_relevance],
+        feedbacks: list[Feedback] = [
+            context_relevance_feedback,
+            groundedness_feedback,
+            answer_relevance_feedback,
+        ]
+
+        session = TruSession()
+        app_id = 'my_app_id'
+        tru = TruVirtual(
+            app=VirtualApp(),
+            app_id=app_id,
+            feedbacks=feedbacks,
         )
+
+        outputs: list[EvaluationOutput] = []
+
+        for i, record in enumerate(virtual_records):
+            tru.add_record(record, FeedbackMode.WITH_APP_THREAD)
+
+            # TODO multiple reasons
+            output = EvaluationOutput(
+                input_id=inputs[i].id,
+                output_entries=[
+                    EvaluationOutputEntry(
+                        metric_name=future_result.name,
+                        score=future_result.result,
+                        reason=None,  # [call.meta['reason'] for call in feedback_result.calls]
+                    )
+                    for future_result in [
+                        future.result() for future in record.feedback_results
+                    ]
+                    if future_result.status == FeedbackResultStatus.DONE
+                ],
+            )
+            outputs.append(output)
+
+        session.delete_app(app_id)
+
+        return outputs
