a3f2fd59adc89064b81a656c9d518c6858f357f1
Luka958 <lp53523@fer.hr>
2025-01-26 23:11:29 +0100

    MlFlowEvaluator WIP

--- libs/community/mlflow/eval_fusion_mlflow/evaluator.py
+++ libs/community/mlflow/eval_fusion_mlflow/evaluator.py
@@ -7,6 +7,16 @@ from eval_fusion_core.models import (
     EvaluationOutput,
     EvaluationOutputEntry,
 )
+from mlflow import evaluate
+from mlflow.metrics.genai import (
+    answer_correctness,
+    answer_relevance,
+    answer_similarity,
+    faithfulness,
+    relevance,
+)
+from mlflow.models.evaluation import EvaluationMetric, EvaluationResult
+from pandas import DataFrame
 
 from .llm import MlFlowProxyLLM
 
@@ -19,4 +29,60 @@ class MlFlowEvaluator(EvalFusionBaseEvaluator):
         self, inputs: list[EvaluationInput], metrics: list
     ) -> list[EvaluationOutput]:
         # TODO organize metrics
-        pass
+
+        model = 'openai:/gpt-4o-mini'  # TODO
+
+        metrics: list[EvaluationMetric] = [
+            faithfulness(model=model),
+            answer_correctness(model=model),
+            answer_relevance(model=model),
+            answer_similarity(model=model),
+            relevance(model=model),
+        ]
+
+        data_frame = DataFrame(
+            [
+                {
+                    'inputs': x.input,
+                    'context': x.relevant_chunks,
+                    'answers': x.output,
+                    'targets': x.ground_truth,
+                }
+                for x in inputs
+            ]
+        )
+
+        evaluation_outputs: list[EvaluationOutput] = []
+
+        for i, (_, series) in enumerate(data_frame.iterrows()):
+            evaluation_output_entires: list[EvaluationOutputEntry] = []
+
+            for metric in metrics:
+                evaluation_result: EvaluationResult = evaluate(
+                    model=None,
+                    data=series.to_frame().T,
+                    model_type=None,
+                    evaluators=None,
+                    predictions='targets',
+                    extra_metrics=[metric],
+                )
+                table = evaluation_result.tables['eval_results_table']
+                score = table[f'{metric.__name__}/v1/score']
+                reason = table[f'{metric.__name__}/v1/justification']
+
+                evaluation_output_entires.append(
+                    EvaluationOutputEntry(
+                        metric_name=metric.__name__,
+                        score=score,
+                        reason=reason,
+                    )
+                )
+
+            evaluation_outputs.append(
+                EvaluationOutput(
+                    input_id=inputs[i].id,
+                    output_entries=evaluation_output_entires,
+                )
+            )
+
+        return evaluation_outputs
