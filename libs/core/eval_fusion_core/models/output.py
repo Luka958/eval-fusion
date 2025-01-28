69d7361709372a81f017ce910e2c9ec35098f130
Luka958 <lp53523@fer.hr>
2025-01-28 22:35:02 +0100

    updated EvaluationOutputEntry

--- libs/core/eval_fusion_core/models/output.py
+++ libs/core/eval_fusion_core/models/output.py
@@ -1,12 +1,35 @@
 from uuid import UUID, uuid4
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, model_validator
 
 
 class EvaluationOutputEntry(BaseModel):
     metric_name: str
-    score: str
+    score: float | None
     reason: str | None
+    error: str | None
+
+    @model_validator(mode='after')
+    def check(cls, model: 'EvaluationOutputEntry') -> 'EvaluationOutputEntry':
+        if model.score is not None:
+            if model.error is not None:
+                raise ValueError('If "score" exists, "error" must not exist.')
+
+        else:
+            if model.reason is not None:
+                raise ValueError('If "score" does not exist, "reason" must not exist.')
+
+            if model.error is None:
+                raise ValueError('If "score" does not exist, "error" must exist.')
+
+        if model.reason is not None:
+            if model.score is None:
+                raise ValueError('If "reason" exists, "score" must also exist.')
+
+            if model.error is not None:
+                raise ValueError('If "reason" exists, "error" must not exist.')
+
+        return model
 
 
 class EvaluationOutput(BaseModel):
