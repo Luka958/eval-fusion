bb7418cfdf3149ebf7a4c150aee50f99aa640ee5
Luka958 <lp53523@fer.hr>
2025-02-16 22:20:05 +0100

    finished LlamaIndexEvaluator

--- libs/community/llama_index/eval_fusion_llama_index/evaluator.py
+++ libs/community/llama_index/eval_fusion_llama_index/evaluator.py
@@ -12,6 +12,7 @@ from eval_fusion_core.models import (
     EvaluationOutputEntry,
 )
 from eval_fusion_core.models.settings import EvalFusionEMSettings, EvalFusionLLMSettings
+from llama_index.core.evaluation import SemanticSimilarityEvaluator
 
 from .em import LlamaIndexProxyEM
 from .llm import LlamaIndexProxyLLM
@@ -41,26 +42,38 @@ class LlamaIndexEvaluator(EvalFusionBaseEvaluator):
             metrics = FEATURE_TO_METRICS[feature]
 
         metric_types = list(map(METRIC_TO_TYPE.get, metrics))
-        metric_instances = [metric_type() for metric_type in metric_types]  # TODO
+        evaluators = [
+            metric_type(self._em)
+            if metric_type == SemanticSimilarityEvaluator
+            else metric_type(self._llm)
+            for metric_type in metric_types
+        ]
 
         outputs: list[EvaluationOutput] = []
 
         for i, input in enumerate(inputs):
             output_entries: list[EvaluationOutputEntry] = []
 
-            for metric in metric_instances:
+            for evaluator in evaluators:
                 metric_name = ...
 
                 try:
                     start = perf_counter()
-                    score = ...
+                    evaluation_result = evaluator.evaluate(
+                        query=input.input,
+                        response=input.output,
+                        contexts=input.relevant_chunks,
+                    )
                     time = perf_counter() - start
+
+                    score = evaluation_result.score
+                    reason = evaluation_result.feedback
 
                     output_entries.append(
                         EvaluationOutputEntry(
                             metric_name=metric_name,
                             score=score,
-                            reason=None,
+                            reason=reason,
                             error=None,
                             time=time,
                         )
